ca740ebd59fb0bada23171a596fa19af570b9387
utk-d <utkrsh.dev@gmail.com>
2024-11-24 20:48:11 +0530

    Responsive - Lp sm

--- src/components/LpBody/aboutUs-4/Us4main.jsx
+++ src/components/LpBody/aboutUs-4/Us4main.jsx
@@ -4,7 +4,7 @@ import Sec2 from './sec-2/Sec2'
 
 const Us4main = () => {
   return (
-    <div className='h-[37vh] im1:w-[92%] flex flex-col relative tab:left-[7.6vh] top-1 md:w-[73.7vh] md:bg-green-300 tab:bg-[#DFDDC3]'>
+    <div className='h-[37vh] im1:w-[92%] flex flex-col relative tab:left-[7.6vh] top-1 md:w-[73.7vh] tab:bg-[#DFDDC3] md:left-[3vw]'>
         <Sec1/>
         <Sec2/>
     </div>
--- src/components/LpBody/aboutUs/AboutImg.jsx
+++ src/components/LpBody/aboutUs/AboutImg.jsx
@@ -7,7 +7,7 @@ const AboutImg = () => {
       <div id='img-3' className=' h-[38vh] bottom-[2vw] w-[18vw] relative invisible im43:visible z-[1] '><img src="../public/image 6.png" alt="" /></div>
       <div id='img-2' className='h-[27vh] w-[27vh]z-[2] invisible i21:visible'><img src="../public/image 4.png" alt="" /></div>
       <div id='img-1' className=' i21:h-96 i21:w-[17vw] lg:-right-[40vw] lg:-top-[0.5vw] lg:h-[2vw] z-[1] i21:-right-[16vw] absolute invisible im1:visible '><img src="../public/image 2.png" alt="" /></div>
-      <div id='pmtbimg' className='w-[25vw] md:left-[71vw] lg:left-[74vw] md:top-[2vw] absolute im1:left-[0vw] im1:invisible  sm:visible '><img src="../public/image 2.png" alt="" /></div>
+      <div id='pmtbimg' className='w-[25vw] md:left-[71vw] lg:left-[74vw] md:top-[2vw] absolute im1:left-[0vw] im1:invisible sm:visible sm:left-[52vh] sm:top-[7vh]'><img src="../public/image 2.png" alt="" /></div>
     </div>
   )
 }
